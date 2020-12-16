b4d4671965d1f0ceca145682e7acc92e30f5655b
김남수 <aowlrehrk3@naver.com>
2020-12-16 13:28:29 +0900

    Entity Class

--- jpashop/src/main/java/jpabook/jpashop/domain/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/MemberRepository.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop;
+package jpabook.jpashop.domain;
 
 import org.springframework.stereotype.Repository;
 
@@ -8,13 +8,11 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {
     @PersistenceContext
-    private EntityManager em;
-
-    public Long save(Member member){
-        em.persist(member);
-        return member.getId();
+    EntityManager em;
+    public Long save(Member member) {
+        em.persist(member); return member.getId();
     }
-    public Member find(Long id){
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
 }
