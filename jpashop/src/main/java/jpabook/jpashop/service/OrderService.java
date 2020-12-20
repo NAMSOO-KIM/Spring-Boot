4310d6d764ebed483bccbdfa8a38ce765740169b
김남수 <aowlrehrk3@naver.com>
2020-12-20 23:44:01 +0900

    order and cancel

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -55,9 +57,10 @@ public class OrderService {
         // 주문 취소
         order.cancel();
     }
-    
+
+
     //검색
-//    public List<Order> findOrders(OrderSearch orderSearch){
-//        return OrderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
